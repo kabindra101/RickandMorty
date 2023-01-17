393ecc6b33aca80b13c526e2f28519be2ee78a24
Kabindra Karki <kkarki@nepaltechsolutions.com>
2023-01-17 23:08:23 +0545

    created api request

--- RickAndMorty/API Client/RMRequest.swift
+++ RickAndMorty/API Client/RMRequest.swift
@@ -9,8 +9,68 @@ import Foundation
 
 /// object that represent a single API call
 final class RMRequest {
-    /// Base Url
+    /// API Constants
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
     
+    ///Desired endpoint
+    private let endpoints: RMEndPoint
     
-    /// End points
+    /// Path component for API, if any
+    private let pathComponent: Set<String>
+    
+    /// Query arguments for API, if any
+    private let queryParameters: [URLQueryItem]
+    
+    /// Constructed url for the api request in string format
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endpoints.rawValue
+        
+        if !pathComponent.isEmpty {
+            pathComponent.forEach({
+                string += "/\($0)"
+            })
+        }
+        
+        if !queryParameters.isEmpty {
+            string += "?"
+            // name=value&name=value
+            let argumentString = queryParameters.compactMap({
+                guard let value = $0.value else { return nil }
+                return "\($0.name)=\(value)"
+            }).joined(separator: "&")
+            
+            string += argumentString
+        }
+        
+        return string
+    }
+    
+    /// Computed and constucted api url
+    public var url: URL? {
+        return URL(string: urlString)
+    }
+    
+    /// Desired http method
+    public let httpMethod = "GET"
+    
+    // MARK: - Public
+    
+    /// Construct request
+    /// - Parameters:
+    ///   - endpoints: Target endpoint
+    ///   - pathComponent: Collection of path component
+    ///   - queryParameters: Collection of query parameters
+    public init(
+        endpoints: RMEndPoint,
+        pathComponent: Set<String> = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endpoints = endpoints
+        self.pathComponent = pathComponent
+        self.queryParameters = queryParameters
+    }
 }
